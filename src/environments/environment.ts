fef97528f7cb039a59f41ef66f4150bbf3a7d364
Malik <abdulmalikxk@gmail.com>
2025-01-23 12:51:32 +0530

    Fix: changes in apiUrl in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:3000',
-    apiUrl: ' https://mkart.shop',
+    apiUrl:'https://easycure-backend.onrender.com',
+    // apiUrl: ' https://mkart.shop',
     RAZORPAY_KEY: "rzp_test_cSwxLAAuCdmhnU"
   };
